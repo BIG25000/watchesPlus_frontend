23a24959f9fe8327d4c5d8fa3a1429990786dc81
seasittipol <charmy_best@hotmail.com>
2024-03-02 12:02:00 +0700

    change card bg

--- src/components/CardProduct.jsx
+++ src/components/CardProduct.jsx
@@ -9,7 +9,7 @@ export default function CardProduct() {
     }
     return (
         <div className="w-72 h-96 flex flex-col bg-black shadow">
-            <div className="relative h-60 bg-gray-200 flex justify-center items-center">
+            <div className="relative h-60 bg-egg flex justify-center items-center">
                 <img className="w-48" src="https://cdn2.chrono24.com/cdn-cgi/image/f=auto,metadata=none,q=65,h=305/images/topmodels/964-t1krledkejovwzq0l6bnygy8-Original.png" />
                 <div className="absolute right-2 top-2" onClick={handleClickLike} role="button">
                     {isLike ? <Heart color="#ff0000" fill="red" /> : <Heart color="#ff0000" />}
