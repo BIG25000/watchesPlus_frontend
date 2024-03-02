68cafb0fcbe2876f3f4e05bd560950a80894c28d
seasittipol <charmy_best@hotmail.com>
2024-03-03 01:58:40 +0700

    add sort by select

---
+++ src/constants/sort.js
@@ -0,0 +1,16 @@
+export const sortProduct = [
+    { id: 1, name: 'New product' },
+    { id: 2, name: 'Price from highest to lowest' },
+    { id: 3, name: 'Price from lowest to highest' },
+    { id: 4, name: 'Popularity' }
+]
+
+export const sortBrand = [
+    { id: 0, name: "All" },
+    { id: 1, name: "Rolex" },
+    { id: 2, name: "Omega" },
+    { id: 3, name: "Seiko" },
+    { id: 4, name: "Cartier" },
+    { id: 5, name: "Hublot" },
+    { id: 6, name: "Casio" }
+]
--- src/features/product/components/SearchProduct.jsx
+++ src/features/product/components/SearchProduct.jsx
@@ -1,11 +1,33 @@
 import CardProduct from './CardProduct'
+import SelectionForm from './SelectionForm'
+import { sortBrand, sortProduct } from '../../../constants/sort'
+import { useState } from 'react'
 
 export default function SearchProduct() {
+    const [selectProduct, setSelectProduct] = useState(null)
+    const [selectBrand, setSelectBrand] = useState(null)
+
+    const handleSelectSortProduct = (e) => {
+        setSelectProduct(e.target.value)
+    }
+
+    const handleSelectSortBrand = (e) => {
+        setSelectBrand(e.target.value)
+    }
     return (
         <div className=" mx-auto w-[1200px] min-h-56 flex flex-col gap-4 mt-4 mb-8">
-            <div className="text-2xl font-bold">Product</div>
-
-            <CardProduct />
+            <div className="text-2xl font-bold">WatchesPlus - Product</div>
+            <div className='flex gap-4 justify-end'>
+                <SelectionForm sort={sortProduct} onChange={handleSelectSortProduct} />
+                <SelectionForm sort={sortBrand} onChange={handleSelectSortBrand} />
+            </div>
+            <div className='flex flex-wrap gap-4'>
+                <CardProduct />
+                <CardProduct />
+                <CardProduct />
+                <CardProduct />
+                <CardProduct />
+            </div>
         </div>
     )
 }
