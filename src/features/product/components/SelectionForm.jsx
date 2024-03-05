2af3a843591376205eecbc9e6e0d2f0b90f088f1
seasittipol <charmy_best@hotmail.com>
2024-03-05 16:55:27 +0700

    finished selection-from

--- src/features/product/components/SelectionForm.jsx
+++ src/features/product/components/SelectionForm.jsx
@@ -1,8 +1,7 @@
-export default function SelectionForm({ name }) {
-    const optionArray = name.split(',')
+export default function SelectionForm({ items, onClick }) {
     return (
-        <select className="outline-none border px-4 py-2 bg-egg">
-            {optionArray.map(el => <option key={el}>{el}</option>)}
+        <select className="outline-none border px-4 py-2 bg-egg" onClick={onClick}>
+            {items?.map(el => <option key={el.id}>{el.name}</option>)}
         </select>
     )
 }
