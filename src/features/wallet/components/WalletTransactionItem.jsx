547aab4105482ddbeeab00897a52b432e7375a8e
ohmmpakpoomm <ohmmpakpoomm@gmail.com>
2024-03-05 21:27:19 +0700

    add case color amount in wallet history

--- src/features/wallet/components/WalletTransactionItem.jsx
+++ src/features/wallet/components/WalletTransactionItem.jsx
@@ -1,12 +1,19 @@
 import React from "react";
+import useWallet from "../../../hooks/useWallet";
 
 export default function WalletTransactionItem({ el }) {
+  const {
+    wallet: { id },
+  } = useWallet();
   return (
     <li className="p-4 border border-b-0 flex ">
       <div className="flex-1">{el.createdAt.split("T")[0]}</div>
       <div
         className={`flex-1 ${
-          el.type === "DEPOSIT" ? "text-green-500" : "text-red-500"
+          el.type === "DEPOSIT" ||
+          (el.type === "TRANSFER" && el.toWalletId === id)
+            ? "text-green-500"
+            : "text-red-500"
         }`}
       >
         {el.price}
