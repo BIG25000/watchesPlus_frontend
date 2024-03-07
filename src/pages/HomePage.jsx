68d71d7ff8294bd1129438417a217d58e183ec33
seasittipol <charmy_best@hotmail.com>
2024-03-07 16:15:52 +0700

    change folder and add get all brand

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,5 @@
 import CardProduct from "../features/product/components/CardProduct"
-import SearchProduct from "../features/product/components/SearchProduct"
+import SearchProduct from "../features/search/components/SearchProduct"
 import MyMarket from "../layouts/MyMarket"
 import useAuth from '../hooks/useAuth'
 import VideoCarousel from "../features/slide/components/VideoSlider"
@@ -9,7 +9,7 @@ export default function HomePage() {
     const { authUser } = useAuth()
     return (
         <div className=" mx-auto w-[1200px] min-h-56 flex flex-col gap-4 mt-4 mb-8">
-            <VideoCarousel/>
+            <VideoCarousel />
             {authUser ? <MyMarket /> : <></>}
             <SearchProduct />
         </div>
