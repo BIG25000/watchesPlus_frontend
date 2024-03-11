3abcd63020b88050cad5141e0accb19157d9225e
redrumkumg <n.ronnapat@github.com>
2024-03-11 20:18:01 +0700

    editDecoraion

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -12,16 +12,17 @@ export default function Input({
 }) {
   return (
     <div className="relative">
-      <div className="absolute left-4 bottom-3">{children}</div>
+      <div className="absolute left-4 bottom-3 ">{children}</div>
       <input
         type={type}
         placeholder={placeholder}
         name={name}
         value={value}
         onChange={onChange}
-        className={` border-white border-2 pl-12 bg-white bg-opacity-0 text-white placeholder:text-white px-6 w-[350px] h-[45px] rounded-2xl focus:outline-none ${extendClassName}`}
+        className={` border-white border-2 pl-12 bg-white bg-opacity-0 hover:border-brown hover:placeholder:text-brown text-white placeholder:text-white px-6 w-[350px] h-[45px] rounded-2xl focus:outline-none ${extendClassName}`}
         onKeyDown={onKeyDown}
       />
     </div>
   );
 }
+
--- src/layouts/DropdownItem.jsx
+++ src/layouts/DropdownItem.jsx
@@ -5,12 +5,12 @@ export default function DropdownItem({ icon, name, link, onClick }) {
   return (
     <>
       <Link to={link}>
-        <div role="button" className="flex gap-2 px-3 py-2" onClick={onClick}>
+        <div role="button" className="flex bg-brown gap-2 px-3 py-2 hover:bg-amber-900" onClick={onClick}>
           <Icon name={icon} />
           <span>{name}</span>
         </div>
       </Link>
-      <hr className="border border-brown" />
+      <hr className="border border-yellow-950" />
     </>
   );
 }
