d6abe7f0c4049b554d4c122d59aa08030d72b5ad
kvinbindai <kavinsanguansook@gmail.com>
2024-03-12 17:24:45 +0700

    add before merge

--- src/features/product/components/OrderContainer.jsx
+++ src/features/product/components/OrderContainer.jsx
@@ -6,27 +6,46 @@ import Button from "../../../components/Button";
 import Loading from "../../../components/Loading";
 
 export default function OrderContainer(props) {
-  const { children, id, dataBuy, dataSale , watch , setLoading , loading } = props;
+  const { children, id, dataBuy, dataSale, watch, setLoading, loading } = props;
 
   return (
     <div className="border-solid border-2 w-96 border-black text-center px-6 py-4 rounded-2xl">
       {/* header */}
       <div className="text-lg flex flex-col gap-8 items-center">
-        {dataBuy?.length > 0 && (
+        {dataBuy?.length > 0 ? (
           <div className="flex gap-2">
-            <span className="font-bold">{dataBuy?.length || 0}</span>
-            <span>รายการขาย</span>
-            <span>เริ่มต้น</span>
-            <span className="font-bold">{(baht +' '+dataBuy[0]?.price) || 0}</span>
+            <>
+              <span className="font-bold">{dataBuy?.length || 0}</span>
+              <span>Order</span>
+              <span>At Start On Price</span>
+              <span className="font-bold">
+                {baht + " " + dataBuy[0]?.price || 0}
+              </span>
+            </>
           </div>
+        ) : (
+          <>
+          {
+            id === 'buy' ? <h1>No Buy Order on This Watch</h1> : null
+          }
+          </>
         )}
-        {dataSale?.length > 0 && (
+        {dataSale?.length > 0  ? (
           <div className="flex gap-2">
             <span className="font-bold">{dataSale?.length || 0}</span>
-            <span>รายการขาย</span>
-            <span>เริ่มต้น</span>
-            <span className="font-bold">{(baht +' '+dataSale[0]?.price) || 0}</span>
+            <span>Order</span>
+              <span>At Start On Price</span>
+            <span className="font-bold">
+              {baht + " " + dataSale[0]?.price || 0}
+            </span>
           </div>
+        ) : (
+          <>
+          {
+            id === 'sell' ? <h1>No Sale Order on This Watch</h1> : null
+          }
+          </>
+          
         )}
         <Button
           onClick={() => document.getElementById(id).showModal()}
@@ -42,37 +61,60 @@ export default function OrderContainer(props) {
         <table className="table table-zebra">
           <thead>
             <tr>
-              <th>ราคา</th>
-              <th>จำนวน</th>
+              <th>Price</th>
+              <th>Amount</th>
             </tr>
           </thead>
           <tbody>
-            {dataBuy?.length > 0 && id === 'buy' ? dataBuy?.map((e,index) => {
-              if(index < 6){
-                return (
-                  <tr key={e.id}>
-                    <td>{`${baht} ${e.price}`}</td>
-                    <td>1</td>
+            {dataBuy?.length > 0 && id === "buy"
+              ? dataBuy?.map((e, index) => {
+                  if (index < 6) {
+                    return (
+                      <tr key={e.id}>
+                        <td>{`${baht} ${e.price}`}</td>
+                        <td>1</td>
+                      </tr>
+                    );
+                  }
+                })
+              : id === "buy" && (
+                  <tr>
+                    <td className="text-center" colSpan={2}>
+                      --- No BuyOrder on This Watch ---
+                    </td>
                   </tr>
-                );
-              }
-            }) : id === 'buy' && <tr><td className="text-center" colSpan={2}>--- No BuyOrder on This Watch ---</td></tr>}
-            {dataSale?.length > 0 && id ==='sell' ? dataSale?.map((e,index) => {
-               if(index < 6){
-                return (
-                  <tr key={e.id}>
-                    <td>{`${baht} ${e.price}`}</td>
-                    <td>1</td>
+                )}
+            {dataSale?.length > 0 && id === "sell"
+              ? dataSale?.map((e, index) => {
+                  if (index < 6) {
+                    return (
+                      <tr key={e.id}>
+                        <td>{`${baht} ${e.price}`}</td>
+                        <td>1</td>
+                      </tr>
+                    );
+                  }
+                })
+              : id === "sell" && (
+                  <tr>
+                    <td className="text-center" colSpan={2}>
+                      --- No SaleOrder on This Watch ---
+                    </td>
                   </tr>
-                );
-              }
-            }) : id === 'sell' && <tr><td className="text-center" colSpan={2}>--- No SaleOrder on This Watch ---</td></tr>}
-          
+                )}
           </tbody>
         </table>
       </div>
-      {id === 'buy' ? <BuyModal  watch={watch} dataBuy={dataBuy} setLoading={setLoading} loading={loading}/> : <SellModal watch={watch} dataSale={dataSale} setLoading={setLoading}  />  }
-      
+      {id === "buy" ? (
+        <BuyModal
+          watch={watch}
+          dataBuy={dataBuy}
+          setLoading={setLoading}
+          loading={loading}
+        />
+      ) : (
+        <SellModal watch={watch} dataSale={dataSale} setLoading={setLoading} />
+      )}
     </div>
   );
 }
