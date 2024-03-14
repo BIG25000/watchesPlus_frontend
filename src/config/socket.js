a724adaf501231899927ba1428dd24e1644ce835
ohmmpakpoomm <ohmmpakpoomm@gmail.com>
2024-03-14 17:25:11 +0700

    change socket

--- src/config/socket.js
+++ src/config/socket.js
@@ -1,5 +1,7 @@
 import io from "socket.io-client";
 
-const socket = io.connect("http://localhost:8888", { autoConnect: false });
+const socket = io.connect(import.meta.env.VITE_API_URL, {
+  autoConnect: false,
+});
 
 export default socket;
